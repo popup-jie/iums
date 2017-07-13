2aa1a21dd6a0679d47c68158d9b7577cef706b1b
shenjunjian <13682473985@kapokcloud.com>
2017-07-13 18:38:55 +0800

    push_first

---
+++ MP/js/size.js
@@ -0,0 +1,14 @@
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+			if(clientWidth>750){clientWidth=750}
+           docEl.style.fontSize = 100 * (clientWidth / 1000) + 'px';
+        };
+    // Abort if browser does not support addEventListener
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
